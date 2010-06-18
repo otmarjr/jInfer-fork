b2fb884f87d5e190e51cde0b09d073de27f79cad
vektor330 <vektor330@gmail.com>
2010-06-18 08:59:48 +0000

    TrivialSimplifier: renamed to ModularSimplifier.

--- jinfer/trunk/jInfer/ModularSimplifier/test/unit/src/cz/cuni/mff/ksi/jinfer/trivialsimplifier/CPTrieTest.java
+++ jinfer/trunk/jInfer/ModularSimplifier/test/unit/src/cz/cuni/mff/ksi/jinfer/trivialsimplifier/CPTrieTest.java
@@ -15,9 +15,9 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package cz.cuni.mff.ksi.jinfer.modularsimplifier;
+package cz.cuni.mff.ksi.jinfer.trivialsimplifier;
 
-import cz.cuni.mff.ksi.jinfer.modularsimplifier.processing.CPTrie;
+import cz.cuni.mff.ksi.jinfer.trivialsimplifier.processing.CPTrie;
 import cz.cuni.mff.ksi.jinfer.base.objects.AbstractNode;
 import cz.cuni.mff.ksi.jinfer.base.objects.Element;
 import cz.cuni.mff.ksi.jinfer.base.regexp.Regexp;

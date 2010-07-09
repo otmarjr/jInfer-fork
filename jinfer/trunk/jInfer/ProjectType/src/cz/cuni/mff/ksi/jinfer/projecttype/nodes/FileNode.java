a10d94fde217f7a4f42317126ee27a68fbc82808
svirooo <svirooo@214d1e34-ff19-46a5-8cc8-ebf7b210affd>
2010-07-09 19:20:24 +0000

    changed default delete action of input files. Now file is not deleted from disk only from our input files model.

---
+++ jinfer/trunk/jInfer/ProjectType/src/cz/cuni/mff/ksi/jinfer/projecttype/nodes/FileNode.java
@@ -0,0 +1,37 @@
+
+package cz.cuni.mff.ksi.jinfer.projecttype.nodes;
+
+import java.io.IOException;
+import java.util.Collection;
+import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataNode;
+import org.openide.nodes.FilterNode;
+import org.openide.nodes.Node;
+
+/**
+ *
+ * @author sviro
+ */
+public class FileNode extends FilterNode{
+
+
+  public FileNode(Node node) {
+    super(node, new FilterNode.Children(node), node.getLookup());
+    disableDelegation(DELEGATE_DESTROY);
+  }
+
+  @Override
+  public void destroy() throws IOException {
+    Collection<FileObject> files = (Collection<FileObject>)((FolderNode)this.getParentNode()).getLookup().lookup(Collection.class);
+    DataNode orig = (DataNode) this.getOriginal();
+    FileObject fileOb = orig.getDataObject().getPrimaryFile();
+    files.remove(fileOb);
+
+    ((FileChildren)((FolderNode)this.getParentNode()).getChildren()).addNotify();
+    
+    super.destroy();
+  }
+
+  
+
+}

61257dec674dea5d3cdef60a68e788b1bbb411c7
vektor330 <vektor330@214d1e34-ff19-46a5-8cc8-ebf7b210affd>
2010-07-20 09:01:42 +0000

    TrivialIGG: XPath attribute values.

--- jinfer/trunk/jInfer/TrivialIGG/src/cz/cuni/mff/ksi/jinfer/trivialigg/XPathHandlerImpl.java
+++ jinfer/trunk/jInfer/TrivialIGG/src/cz/cuni/mff/ksi/jinfer/trivialigg/XPathHandlerImpl.java
@@ -22,11 +22,10 @@ import cz.cuni.mff.ksi.jinfer.base.objects.Element;
 import cz.cuni.mff.ksi.jinfer.base.regexp.Regexp;
 import cz.cuni.mff.ksi.jinfer.trivialigg.utils.IGGUtils;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.apache.log4j.Logger;
 import org.jaxen.saxpath.Axis;
+import org.jaxen.saxpath.Operator;
 import org.jaxen.saxpath.SAXPathException;
 import org.jaxen.saxpath.helpers.DefaultXPathHandler;
 
@@ -42,18 +41,22 @@ public class XPathHandlerImpl extends DefaultXPathHandler {
   /** Rules that have been inferred so far. */
   private final List<AbstractNode> rules = new ArrayList<AbstractNode>();
   /** The element we were looking at the last time. */
-  private Element lastStep = null;
+  private Element lastElement = null;
+  /** The attribute we were looking at the last time. */
+  private Attribute lastAttribute = null;
+  /** The literal we were looking at the last time. */
+  private String lastLiteral = null;
 
   @Override
   public void startAllNodeStep(final int axis) throws SAXPathException {
     super.startAllNodeStep(axis);
 
     if (axis == Axis.DESCENDANT_OR_SELF || axis == Axis.PARENT) {
-      if (lastStep != null) {
-        rules.add(lastStep);
+      if (lastElement != null) {
+        rules.add(lastElement);
       }
 
-      lastStep = null;
+      lastElement = null;
     }
 
     LOG.info("allNode: " + axis + " (" + Axis.lookup(axis) + ")");
@@ -63,22 +66,49 @@ public class XPathHandlerImpl extends DefaultXPathHandler {
   public void startNameStep(final int axis, final String prefix, final String localName) throws SAXPathException {
     super.startNameStep(axis, prefix, localName);
 
+    // TODO vektor switch
     if (axis == Axis.CHILD) {
-      final Element newStep = new Element(null, localName, IGGUtils.ATTR_FROM_QUERY, Regexp.<AbstractNode>getConcatenation());
-      if (lastStep != null) {
-        lastStep.getSubnodes().addChild(Regexp.<AbstractNode>getToken(newStep));
-        rules.add(lastStep);
+      final Element newElement = new Element(null, localName, IGGUtils.ATTR_FROM_QUERY, Regexp.<AbstractNode>getConcatenation());
+      if (lastElement != null) {
+        lastElement.getSubnodes().addChild(Regexp.<AbstractNode>getToken(newElement));
+        rules.add(lastElement);
       }
-      lastStep = newStep;
-    } else if (axis == Axis.ATTRIBUTE) {
-      if (lastStep != null) {
-        lastStep.getSubnodes().addChild(Regexp.<AbstractNode>getToken(
-                new Attribute(null, localName, IGGUtils.ATTR_FROM_QUERY, null, new ArrayList<String>(0))));
-        rules.add(lastStep);
-      }
+      lastElement = newElement;
+      lastAttribute = null;
+    } else if (axis == Axis.ATTRIBUTE && lastElement != null) {
+      final Attribute newAttr = new Attribute(null, localName, IGGUtils.ATTR_FROM_QUERY, null, new ArrayList<String>(0));
+      lastElement.getSubnodes().addChild(Regexp.<AbstractNode>getToken(newAttr));
+      lastAttribute = newAttr;
+      rules.add(lastElement);
     }
 
     LOG.info("name: " + axis + " (" + Axis.lookup(axis) + "), " + prefix + ", " + localName);
+  }
+
+  @Override
+  public void literal(final String literal) throws SAXPathException {
+    super.literal(literal);
+    lastLiteral = literal;
+    LOG.info("literal: " + literal);
+  }
+
+  @Override
+  public void startEqualityExpr() throws SAXPathException {
+    super.startEqualityExpr();
+    lastLiteral = null;
+    LOG.info("startEqualityExpr");
+  }
+
+  @Override
+  public void endEqualityExpr(final int operator) throws SAXPathException {
+    super.endEqualityExpr(operator);
+
+    if (operator == Operator.EQUALS && lastLiteral != null && lastAttribute != null) {
+      lastAttribute.getContent().add(lastLiteral);
+      lastLiteral = null;
+    }
+
+    LOG.info("endEqualityExpr: " + operator);
   }
 
   /**
@@ -86,10 +116,10 @@ public class XPathHandlerImpl extends DefaultXPathHandler {
    */
   public List<AbstractNode> getRules() {
     final List<AbstractNode> ret = new ArrayList<AbstractNode>(rules);
-    if (lastStep != null) {
-      ret.add(lastStep);
+    if (lastElement != null) {
+      ret.add(lastElement);
     }
-    lastStep = null;
+    lastElement = null;
     return ret;
   }
 }

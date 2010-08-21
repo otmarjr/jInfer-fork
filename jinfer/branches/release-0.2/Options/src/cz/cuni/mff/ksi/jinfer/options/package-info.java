426a2fa10c5d7a6126c945287c90b7eb9725ebd1
vektor330 <vektor330@gmail.com>
2010-08-21 13:13:56 +0000

    Version 0.2 release: ...

--- jinfer/branches/release-0.2/Options/src/cz/cuni/mff/ksi/jinfer/options/package-info.java
+++
@@ -1,2 +0,0 @@
-@org.netbeans.spi.options.OptionsPanelController.ContainerRegistration(id = "jInfer", categoryName = "#OptionsCategory_Name_jInfer", iconBase = "cz/cuni/mff/ksi/jinfer/options/jinfer-icon32.png", keywords = "#OptionsCategory_Keywords_jInfer", keywordsCategory = "jInfer")
-package cz.cuni.mff.ksi.jinfer.options;

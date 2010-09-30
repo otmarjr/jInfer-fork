8ce4e721eed274e8655bb07d4447c34543424d0a
michalklempa <michalklempa@214d1e34-ff19-46a5-8cc8-ebf7b210affd>
2010-09-30 10:46:34 +0000

    doing nasties, moved packages around and trying to not lost them nor make more work than needed

---
+++ jinfer/trunk/jInfer/CrudeMDL/src/cz/cuni/mff/ksi/jinfer/crudemdl/implementations/clusterprocessing/automatonmergingstate/ClusterProcessorAutomatonMergingStateFactory.java
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (C) 2010 anti
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cz.cuni.mff.ksi.jinfer.crudemdl.implementations.clusterprocessing.automatonmergingstate;
+
+import cz.cuni.mff.ksi.jinfer.base.objects.AbstractNode;
+import cz.cuni.mff.ksi.jinfer.crudemdl.clusterprocessing.ClusterProcessor;
+import cz.cuni.mff.ksi.jinfer.crudemdl.clusterprocessing.ClusterProcessorFactory;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ * TODO anti Comment!
+ *
+ * @author anti
+ */
+@ServiceProvider(service = ClusterProcessorFactory.class)
+public class ClusterProcessorAutomatonMergingStateFactory implements ClusterProcessorFactory {
+  @Override
+  public String getName() {
+    return "ClusterProcessorAutomatonMergingState";
+  }
+
+  @Override
+  public String getModuleDescription() {
+    return getName();
+  }
+
+  @Override
+  public ClusterProcessor<AbstractNode> create() {
+    return new ClusterProcessorAutomatonMergingState();
+  }
+}

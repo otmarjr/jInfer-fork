440b410c48b559a093ea2aff6c4c919070acea90
svirooo <svirooo@214d1e34-ff19-46a5-8cc8-ebf7b210affd>
2011-01-31 22:26:14 +0000

    #3168753 added jinfer option position to 0(first)

--- jinfer/trunk/jInfer/Options/src/cz/cuni/mff/ksi/jinfer/options/package-info.java
+++ jinfer/trunk/jInfer/Options/src/cz/cuni/mff/ksi/jinfer/options/package-info.java
@@ -2,5 +2,5 @@
  * Main package of the Options module. This module creates an empty top-level
  * category "jInfer" in the NetBeans configuration window.
  */
-@org.netbeans.spi.options.OptionsPanelController.ContainerRegistration(id = "jInfer", categoryName = "#OptionsCategory_Name_jInfer", iconBase = "cz/cuni/mff/ksi/jinfer/options/jinfer-icon32.png", keywords = "#OptionsCategory_Keywords_jInfer", keywordsCategory = "jInfer")
+@org.netbeans.spi.options.OptionsPanelController.ContainerRegistration(id = "jInfer", categoryName = "#OptionsCategory_Name_jInfer", iconBase = "cz/cuni/mff/ksi/jinfer/options/jinfer-icon32.png", keywords = "#OptionsCategory_Keywords_jInfer", keywordsCategory = "jInfer", position = 0)
 package cz.cuni.mff.ksi.jinfer.options;

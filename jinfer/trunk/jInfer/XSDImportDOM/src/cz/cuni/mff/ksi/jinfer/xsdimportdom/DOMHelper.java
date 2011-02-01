e71f0c0e30603d41b7dc2d85c988b3fdf5f7f361
reseto <reseto@214d1e34-ff19-46a5-8cc8-ebf7b210affd>
2011-02-01 18:05:22 +0000

    Refactoring XSDI:DOM part 2. More on the way.

--- jinfer/trunk/jInfer/XSDImportDOM/src/cz/cuni/mff/ksi/jinfer/xsdimportdom/DOMHelper.java
+++ jinfer/trunk/jInfer/XSDImportDOM/src/cz/cuni/mff/ksi/jinfer/xsdimportdom/DOMHelper.java
@@ -17,16 +17,20 @@
 
 package cz.cuni.mff.ksi.jinfer.xsdimportdom;
 
+import cz.cuni.mff.ksi.jinfer.base.objects.nodes.AbstractStructuralNode;
 import cz.cuni.mff.ksi.jinfer.base.objects.nodes.Element;
 import cz.cuni.mff.ksi.jinfer.base.objects.nodes.SimpleData;
+import cz.cuni.mff.ksi.jinfer.base.regexp.Regexp;
 import cz.cuni.mff.ksi.jinfer.base.regexp.RegexpInterval;
 import cz.cuni.mff.ksi.jinfer.base.regexp.RegexpType;
 import cz.cuni.mff.ksi.jinfer.xsdimporter.utils.XSDAttribute;
+import cz.cuni.mff.ksi.jinfer.xsdimporter.utils.XSDException;
 import cz.cuni.mff.ksi.jinfer.xsdimporter.utils.XSDOccurences;
 import cz.cuni.mff.ksi.jinfer.xsdimporter.utils.XSDTag;
 import cz.cuni.mff.ksi.jinfer.xsdimporter.utils.XSDUtility;
 import java.util.Collections;
 import java.util.List;
+import java.util.MissingResourceException;
 import org.apache.log4j.Logger;
 import org.openide.util.NbBundle;
 import org.w3c.dom.Node;
@@ -69,15 +73,57 @@ public final class DOMHelper {
   }
 
   /**
-   * Helper method for retrieving error value when tags are not nested correctly.
+   * Helper method for retrieving error message when tags are not nested correctly.
    * @param child Tag of the child node.
    * @param parent Tag of the parent node.
    * @return Error message.
    */
-  protected static String errorWrongNested(final XSDTag child, final XSDTag parent) {
+  protected static String errorWrongNested(final XSDTag child, final XSDTag parent) throws MissingResourceException {
     return NbBundle.getMessage(DOMHelper.class, "Error.WrongTagsNested", child.toString(), parent.toString());
   }
 
+  /**
+   * Helper method for retrieving warning message when unsupported structure occurs in schema.
+   * @param child Tag of the child node.
+   * @param parent Tag of the parent node.
+   * @return Warning message.
+   */
+  protected static String warnUnsupported(final XSDTag child, final XSDTag parent) throws MissingResourceException {
+    return NbBundle.getMessage(DOMHelper.class, "Warn.UnsupportedStructure", child.toString(), parent.toString());
+  }
+
+  /**
+   * Extract subnodes and attributes from subtree stored in container and put them directly under
+   * the destination element. This is a helper method for omitting containers.
+   * @param subtree Container element, from which the subnodes will be extracted.
+   * @param destination Destination element, where the subnodes are copied to.
+   * @param containerType Expected name of the subtree element, this is just a consistency check.
+   * @throws XSDException
+   */
+  protected static void extractSubnodesFromContainer(final Element subtree, final Element destination, final String containerType) throws XSDException {
+    if (!subtree.getName().equals(containerType)) {
+      throw new XSDException(NbBundle.getMessage(DOMHandler.class, "Error.WrongContainer", containerType, subtree.getName()));
+    }
+    // compare types of subnodes, set correct type if destination is null
+    // throw exception if types don't match, or do nothing if they match
+    if (destination.getSubnodes().getType() == null && subtree.getSubnodes().getType() != null) {
+      destination.getSubnodes().setType(subtree.getSubnodes().getType());
+    } else if (destination.getSubnodes().getType() != null
+               && subtree.getSubnodes().getType() != null
+               && destination.getSubnodes().getType() != subtree.getSubnodes().getType()) {
+      //                                              ^^ comparing enums
+      // if the two types are not null and not equal, the schema is invalid (mixing wrong types)
+      final String msg = NbBundle.getMessage(DOMHandler.class, "Error.MismatchedTypes",
+        subtree.getSubnodes().getType(), subtree.getName(),
+        destination.getSubnodes().getType(), destination.getName());
+      throw new XSDException(msg);
+    }
+    for (Regexp<AbstractStructuralNode> regexp : subtree.getSubnodes().getChildren()) {
+      destination.getSubnodes().addChild(regexp);
+    }
+    destination.getSubnodes().setInterval(subtree.getSubnodes().getInterval());
+    destination.getAttributes().addAll(subtree.getAttributes());
+  }
 
   /**
    * Check if element is properly defined; redefine it to lambda when it was empty,
@@ -110,7 +156,7 @@ public final class DOMHelper {
     } else if (ret.getSubnodes().getType() == null) {
       // type of element must be non-null, but apparently it has some children
       // this should not happen
-      LOG.warn(NbBundle.getMessage(DOMHandler.class, "Warn.SetDefaultType", ret.getName()));
+      LOG.warn(NbBundle.getMessage(DOMHelper.class, "Warn.SetDefaultType", ret.getName()));
       ret.getSubnodes().setType(RegexpType.CONCATENATION);
       ret.getSubnodes().setContent(null);
     }

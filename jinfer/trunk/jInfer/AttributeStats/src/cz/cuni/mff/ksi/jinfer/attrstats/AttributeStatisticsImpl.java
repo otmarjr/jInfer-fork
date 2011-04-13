479609521027ddddf3a90a9d21eed8683c342527
vektor330 <vektor330@gmail.com>
2011-04-13 08:33:43 +0000

    #3253099 Create "Attribute statistics" module - initial commit.

---
+++ jinfer/trunk/jInfer/AttributeStats/src/cz/cuni/mff/ksi/jinfer/attrstats/AttributeStatisticsImpl.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2011 vektor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cuni.mff.ksi.jinfer.attrstats;
+
+import cz.cuni.mff.ksi.jinfer.base.interfaces.AttributeStatistics;
+import cz.cuni.mff.ksi.jinfer.base.objects.nodes.Element;
+import java.util.List;
+
+/**
+ *
+ * @author vektor
+ */
+public class AttributeStatisticsImpl implements AttributeStatistics {
+
+  private static final String NAME = "AttributeStatistics";
+  private static final String DISPLAY_NAME = "Attribute Statistics";
+  
+  @Override
+  public void showStatistics(final String panelName, final List<Element> grammar) {
+//    final AttrStatsTopComponent topComponent = AttrStatsTopComponent.findInstance();
+//    if (!topComponent.isOpened()) {
+//      topComponent.open();
+//    }
+  }
+
+  @Override
+  public String getName() {
+    return NAME;
+  }
+
+  @Override
+  public String getDisplayName() {
+    return DISPLAY_NAME;
+  }
+
+  @Override
+  public String getModuleDescription() {
+    return getDisplayName();
+  }
+  
+}

4f0333a4d1cd779354e503783550fddfac8cc7c3
Michal Klempa <mkl-eea-sk@users.noreply.github.com>
2011-06-01 10:13:13 +0000

    repairing last commit, added the classes relevant:D

---
+++ jinfer/trunk/jInfer/TwoStepSimplifier/src/cz/cuni/mff/ksi/jinfer/twostep/processing/automatonmergingstate/simplifying/khgrams/KHgramsFactory.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2011 anti
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cuni.mff.ksi.jinfer.twostep.processing.automatonmergingstate.simplifying.khgrams;
+
+import cz.cuni.mff.ksi.jinfer.twostep.processing.automatonmergingstate.simplifying.AutomatonSimplifier;
+import cz.cuni.mff.ksi.jinfer.twostep.processing.automatonmergingstate.simplifying.AutomatonSimplifierFactory;
+import java.util.Collections;
+import java.util.List;
+import org.GNOME.Accessibility.Collection;
+import org.apache.log4j.Logger;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ * TODO anti Comment!
+ *
+ * @author anti
+ */
+@ServiceProvider(service = AutomatonSimplifierFactory.class)
+public class KHgramsFactory implements AutomatonSimplifierFactory {
+  private static final Logger LOG = Logger.getLogger(KHgramsFactory.class);
+  /**
+   * Canonical name.
+   */
+  public static final String NAME = "TwoStepClusterProcessorAutomatonMergingStateAutomatonSimplifierKHgrams";
+  /**
+   * Name presented to user.
+   */
+  public static final String DISPLAY_NAME = "k,h-grams";
+  
+  @Override
+  public <T> AutomatonSimplifier<T> create() {
+    LOG.debug("Creating new " + NAME);
+    return new KHgrams<T>();
+  }
+
+  @Override
+  public String getName() {
+    return NAME;
+  }
+
+  @Override
+  public String getDisplayName() {
+    return DISPLAY_NAME;
+  }
+
+  @Override
+  public String getModuleDescription() {
+    return getName();
+  }
+
+  @Override
+  public List<String> getCapabilities() {
+    return Collections.<String>emptyList();
+  }
+
+  @Override
+  public String getUserModuleDescription() {
+    return "Simplify automaton to fully k,h-context automaton in linear time.";
+  }
+}

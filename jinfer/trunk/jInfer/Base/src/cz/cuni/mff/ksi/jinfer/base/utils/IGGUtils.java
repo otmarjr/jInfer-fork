fce88678615b08d414c973bedd3830667eb49519
vektor330 <vektor330@214d1e34-ff19-46a5-8cc8-ebf7b210affd>
2011-06-03 13:09:01 +0000

    BasicIGG: IG rules now know which file they come from.

--- jinfer/trunk/jInfer/Base/src/cz/cuni/mff/ksi/jinfer/base/utils/IGGUtils.java
+++ jinfer/trunk/jInfer/Base/src/cz/cuni/mff/ksi/jinfer/base/utils/IGGUtils.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright (C) 2010 vektor
- * 
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -44,6 +44,8 @@ public final class IGGUtils {
   public static final String IS_SENTINEL = "is.sentinel";
   /** Metadata string meaning "Attribute is required". */
   public static final String REQUIRED = "required";
+  /** Metadata representing the name of the file where this node originated from. */
+  public static final String FILE_ORIGIN = "file.origin";
 
   /** Metadata map containing a single piece of metadata: "Node comes from a schema". */
   public static final Map<String, Object> ATTR_FROM_SCHEMA = new HashMap<String, Object>(1);

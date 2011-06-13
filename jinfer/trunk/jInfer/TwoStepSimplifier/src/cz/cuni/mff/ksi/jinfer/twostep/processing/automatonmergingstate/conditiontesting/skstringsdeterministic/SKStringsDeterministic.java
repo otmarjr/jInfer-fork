eabe084b040645e5a61d86ee055e4830a600a663
michalklempa <michalklempa@214d1e34-ff19-46a5-8cc8-ebf7b210affd>
2011-06-13 19:00:36 +0000

    determinizer

---
+++ jinfer/trunk/jInfer/TwoStepSimplifier/src/cz/cuni/mff/ksi/jinfer/twostep/processing/automatonmergingstate/conditiontesting/skstringsdeterministic/SKStringsDeterministic.java
@@ -0,0 +1,80 @@
+/*
+ *  Copyright (C) 2010 anti
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cuni.mff.ksi.jinfer.twostep.processing.automatonmergingstate.conditiontesting.skstringsdeterministic;
+
+import cz.cuni.mff.ksi.jinfer.twostep.processing.automatonmergingstate.conditiontesting.MergeConditionTester;
+import cz.cuni.mff.ksi.jinfer.base.automaton.Automaton;
+import cz.cuni.mff.ksi.jinfer.base.automaton.State;
+import cz.cuni.mff.ksi.jinfer.base.automaton.Step;
+import cz.cuni.mff.ksi.jinfer.twostep.ModuleParameters;
+import cz.cuni.mff.ksi.jinfer.twostep.processing.automatonmergingstate.conditiontesting.deterministic.DeterministicFactory;
+import cz.cuni.mff.ksi.jinfer.twostep.processing.automatonmergingstate.conditiontesting.skstrings.SKStringsFactory;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * k,h-context equivalence criterion implementation.
+ *
+ * It first finds all contexts of two states, then compares each pair. Returns
+ * list of alternative merge lists of pairs.
+ *
+ * Not using k-grams algorithm.
+ * 
+ * @author anti
+ */
+public class SKStringsDeterministic<T> implements MergeConditionTester<T> {
+
+  private MergeConditionTester<T> deterministic;
+  private MergeConditionTester<T> skstrings;
+
+  /**
+   * Setting k,h. It has to be k >= h, or exception thrown (cannot merge more than
+   * k states on path, when only k of them were examined).
+   * @param k
+   * @param h
+   */
+  public SKStringsDeterministic(final int k, final double s, final String strategy) {
+    if (!((k > 0)&&(s >= 0)&&(s <= 1))) {
+      throw new IllegalArgumentException("Parameter k must be greater than 0. Parameter s must satisfy: 0 <= s <= 1.");
+    }
+    this.deterministic= (new DeterministicFactory()).create();
+    final SKStringsFactory mergeConditionTesterFactory = new SKStringsFactory();
+    final ModuleParameters factoryParam = ((ModuleParameters) mergeConditionTesterFactory);
+
+    factoryParam.setParameter("k", String.valueOf(k));
+    factoryParam.setParameter("s", String.valueOf(s));
+    factoryParam.setParameter("strategy", strategy);
+
+    this.skstrings= mergeConditionTesterFactory.create();
+  }
+
+
+  @Override
+  public List<List<List<State<T>>>> getMergableStates(final State<T> state1, final State<T> state2, final Automaton<T> automaton) {
+    List<List<List<State<T>>>> alternatives = deterministic.getMergableStates(state1, state2, automaton);
+    if (!alternatives.isEmpty()) {
+      return alternatives;
+    }
+    alternatives = this.skstrings.getMergableStates(state1, state2, automaton);
+    return alternatives;
+  }
+}
+
---
+++ jinfer/trunk/jInfer/TwoStepSimplifier/src/cz/cuni/mff/ksi/jinfer/twostep/processing/automatonmergingstate/conditiontesting/skstringsdeterministic/SKStringsDeterministicFactory.java
@@ -0,0 +1,151 @@
+/*
+ *  Copyright (C) 2010 anti
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cuni.mff.ksi.jinfer.twostep.processing.automatonmergingstate.conditiontesting.skstringsdeterministic;
+
+import cz.cuni.mff.ksi.jinfer.base.utils.RunningProject;
+import cz.cuni.mff.ksi.jinfer.twostep.ModuleParameters;
+import cz.cuni.mff.ksi.jinfer.twostep.processing.automatonmergingstate.conditiontesting.MergeConditionTesterFactory;
+import cz.cuni.mff.ksi.jinfer.twostep.processing.automatonmergingstate.conditiontesting.MergeConditionTester;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.log4j.Logger;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ * Factory for {@link SKStringsDeterministic}.
+ *
+ * Implements "parameters" capability {@see ModuleParameters}.
+ * 
+ * @author anti
+ */
+@ServiceProvider(service = MergeConditionTesterFactory.class)
+public class SKStringsDeterministicFactory implements MergeConditionTesterFactory {
+
+  private static final Logger LOG = Logger.getLogger(SKStringsDeterministicFactory.class);
+  private int parameterK = -1;
+  private double parameterS = -1.0;
+  private String parameterStrategy = "OR";
+  private static final int K_DEFAULT_VALUE = 2;
+  private static final double S_DEFAULT_VALUE = 1.0;
+  private static final String STRATEGY_DEFAULT_VALUE = "OR";
+  /**
+   * Canonical name.
+   */
+  public static final String NAME = "TwoStepClusterProcessorAutomatonMergingStateMergeConditionTesterSKStringsDeterministic";
+  /**
+   * Name presented to user.
+   */
+  public static final String DISPLAY_NAME = "s,k-strings (deterministic)";
+
+  @Override
+  public <T> MergeConditionTester<T> create() {
+    LOG.debug("Creating new " + NAME);
+    if ((parameterS >= 0) && (parameterS <= 100) && (parameterK > 0)
+            && (("OR".equals(parameterStrategy)) || ("AND".equals(parameterStrategy)))) {
+      return new SKStringsDeterministic<T>(parameterK, parameterS, parameterStrategy);
+    } else {
+      LOG.warn("Wrong parameters set k: "
+              + parameterK
+              + ", s: "
+              + parameterS
+              + ". Parameters have to satisfy: k > 0; 0 <= s <= 1. Strategy has to be"
+              + " one of: AND, OR."
+              + " Using default values of k = " + K_DEFAULT_VALUE
+              + ", s = " + S_DEFAULT_VALUE + ", strategy = " + STRATEGY_DEFAULT_VALUE + ".");
+      return new SKStringsDeterministic<T>(K_DEFAULT_VALUE, S_DEFAULT_VALUE, STRATEGY_DEFAULT_VALUE);
+    }
+  }
+
+  @Override
+  public String getName() {
+    return NAME;
+  }
+
+  @Override
+  public String getModuleDescription() {
+    return getDisplayName();
+  }
+
+  @Override
+  public List<String> getCapabilities() {
+    return Arrays.asList("parameters");
+  }
+
+  @Override
+  public String getUserModuleDescription() {
+    final StringBuilder sb = new StringBuilder(getDisplayName());
+    sb.append(" finds all <i>s,k</i>-strings of states being tested. States"
+            + " are considered mergable (<i>s,k</i>-strings equivalent) when <i>s</i> percent of"
+            + "<i>k</i>-strings (tails up to lenght <i>k</i>) are same (<b>AND</b> strategy), or are subset"
+            + " of another state <i>k</i>-tails (<b>OR</b> strategy).");
+    return sb.toString();
+  }
+
+  @Override
+  public List<String> getParameterNames() {
+    return Arrays.<String>asList("k", "s", "strategy");
+  }
+
+  @Override
+  public String getParameterDisplayDescription(final String parameterName) {
+    if ("k".equals(parameterName)) {
+      return "k in <i>s,k</i>-strings. That is the number of transitions that have to"
+              + "be same (by means of symbols) after state (<i>k</i>-tail). Default value: 2.";
+    }
+    if ("s".equals(parameterName)) {
+      return "s in <i>s,k</i>-strings. States are equivalent when <i>s</i> percent of all <i>k</i>-tails are equivalent."
+              + " Default value: 1.0";
+    }
+    if ("strategy".equals(parameterName)) {
+      return "Strategy: <b>OR</b> - <i>s</i> percent of one state <i>k</i>-tails are subset of another state <i>k</i>-tails. <br>"
+              + " Default value: OR.";
+      // TODO anti comment
+    }
+    throw new IllegalArgumentException("Asking for a description of unknown parameter");
+  }
+
+  @Override
+  public void setParameter(final String parameterName, final String newValue) {
+    if ("k".equals(parameterName)) {
+      this.parameterK = Integer.parseInt(newValue);
+    }
+    if ("s".equals(parameterName)) {
+      this.parameterS = Double.parseDouble(newValue);
+    }
+    if ("strategy".equals(parameterName)) {
+      this.parameterStrategy = newValue;
+    }
+  }
+
+  @Override
+  public String getDisplayName() {
+    return DISPLAY_NAME;
+  }
+
+  @Override
+  public String getParameterDefaultValue(final String parameterName) {
+    if ("k".equals(parameterName)) {
+      return String.valueOf(K_DEFAULT_VALUE);
+    } else if ("s".equals(parameterName)) {
+      return String.valueOf(S_DEFAULT_VALUE);
+    } else if ("strategy".equals(parameterName)) {
+      return String.valueOf(STRATEGY_DEFAULT_VALUE);
+    } else {
+      return "";
+    }
+  }
+}

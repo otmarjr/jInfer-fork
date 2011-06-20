4c04314be18f26ae68b88c12524e376da4edcdf7
vektor330 <vektor330@gmail.com>
2011-06-20 15:14:04 +0000

    #3323201 Experimental framework

---
+++ jinfer/trunk/jInfer/AttributeStats/src/cz/cuni/mff/ksi/jinfer/attrstats/experiments/interfaces/Quality.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2011 vektor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cuni.mff.ksi.jinfer.attrstats.experiments.interfaces;
+
+/**
+ * TODO vektor Comment!
+ *
+ * @author vektor
+ */
+public interface Quality {
+
+  // TODO vektor What else should be here?
+
+  /**
+   * Returns a scalar representation of the quality of the solution.
+   *
+   * @return Scalar representation of the quality of the solution.
+   */
+  double getScalar();
+
+  /**
+   * Returns a flag whether this solution is know to be optimal.
+   *
+   * @return <code>True</code> if this solution is known to be optimal,
+   * <code>false</code> otherwise.
+   */
+  boolean isOptimal();
+
+}
---
+++ jinfer/trunk/jInfer/AttributeStats/src/cz/cuni/mff/ksi/jinfer/attrstats/experiments/interfaces/TerminationCriteria.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2011 vektor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cuni.mff.ksi.jinfer.attrstats.experiments.interfaces;
+
+import cz.cuni.mff.ksi.jinfer.attrstats.objects.IdSet;
+
+/**
+ * TODO vektor Comment!
+ *
+ * @author vektor
+ */
+public interface TerminationCriteria {
+
+  /**
+   * Returns a flag whether to terminate the iterations of the metaheuristic.
+   *
+   * @param time Total time taken so far.
+   * @param solution The latest solution that was produced.
+   * @return <code>True</code> if the metaheuristic should be terminated,
+   * <code>false<code> otherwise.
+   */
+  boolean terminate(final long time, final IdSet solution);
+
+}

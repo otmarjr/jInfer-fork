5d220ed9260795c442f67149b16bd56fb9799af8
vektor330 <vektor330@214d1e34-ff19-46a5-8cc8-ebf7b210affd>
2011-06-21 20:58:06 +0000

    #3323201 Experimental framework - in progress

---
+++ jinfer/trunk/jInfer/AttributeStats/src/cz/cuni/mff/ksi/jinfer/attrstats/experiments/ExperimentFactory.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2011 vektor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cuni.mff.ksi.jinfer.attrstats.experiments;
+
+import cz.cuni.mff.ksi.jinfer.attrstats.experiments.interfaces.ImprovementHeuristic;
+import cz.cuni.mff.ksi.jinfer.attrstats.experiments.quality.Weight;
+import cz.cuni.mff.ksi.jinfer.attrstats.experiments.termination.TimeIterations;
+import cz.cuni.mff.ksi.jinfer.attrstats.heuristics.construction.glpk.Glpk;
+import cz.cuni.mff.ksi.jinfer.attrstats.heuristics.improvement.Identity;
+import java.util.Arrays;
+
+/**
+ * TODO vektor Comment!
+ *
+ * @author vektor
+ */
+public final class ExperimentFactory {
+
+  private ExperimentFactory() {
+  }
+
+  public static Experiment createExperiment(final String fileName) {
+    return new Experiment(fileName,
+            new Glpk(1, 1, 3),
+            Arrays.<ImprovementHeuristic>asList(new Identity()),
+            new Weight(1, 1),
+            new TimeIterations(10, 10000000));
+  }
+
+}

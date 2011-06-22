2529c71810ab577f4a483cb3ef5fd55f2c039e70
vektor330 <vektor330@gmail.com>
2011-06-22 17:44:53 +0000

    #3323201 Experimental framework - in progress

--- jinfer/trunk/jInfer/AttributeStats/src/cz/cuni/mff/ksi/jinfer/attrstats/experiments/ExperimentFactory.java
+++ jinfer/trunk/jInfer/AttributeStats/src/cz/cuni/mff/ksi/jinfer/attrstats/experiments/ExperimentFactory.java
@@ -20,7 +20,8 @@ import cz.cuni.mff.ksi.jinfer.attrstats.experiments.interfaces.ImprovementHeuris
 import cz.cuni.mff.ksi.jinfer.attrstats.experiments.quality.Weight;
 import cz.cuni.mff.ksi.jinfer.attrstats.experiments.termination.TimeIterations;
 import cz.cuni.mff.ksi.jinfer.attrstats.heuristics.construction.glpk.Glpk;
-import cz.cuni.mff.ksi.jinfer.attrstats.heuristics.improvement.Identity;
+import cz.cuni.mff.ksi.jinfer.attrstats.heuristics.improvement.Hungry;
+import cz.cuni.mff.ksi.jinfer.attrstats.heuristics.improvement.RandomRemove;
 import java.util.Arrays;
 
 /**
@@ -34,11 +35,11 @@ public final class ExperimentFactory {
   }
 
   public static Experiment createExperiment(final String fileName) {
-    return new Experiment(fileName,
-            new Glpk(1, 1, 3),
-            Arrays.<ImprovementHeuristic>asList(new Identity()),
+    return new Experiment(fileName, 1,
+            new Glpk(1, 1, 1),
+            Arrays.<ImprovementHeuristic>asList(new Hungry(), new RandomRemove(0.3)),
             new Weight(1, 1),
-            new TimeIterations(10, 10000000));
+            new TimeIterations(100, 100000));
   }
 
 }
--- jinfer/trunk/jInfer/AttributeStats/src/cz/cuni/mff/ksi/jinfer/attrstats/heuristics/improvement/RandomRemove.java
+++ jinfer/trunk/jInfer/AttributeStats/src/cz/cuni/mff/ksi/jinfer/attrstats/heuristics/improvement/RandomRemove.java
@@ -19,7 +19,9 @@ package cz.cuni.mff.ksi.jinfer.attrstats.heuristics.improvement;
 import cz.cuni.mff.ksi.jinfer.attrstats.experiments.interfaces.HeuristicCallback;
 import cz.cuni.mff.ksi.jinfer.attrstats.experiments.interfaces.ImprovementHeuristic;
 import cz.cuni.mff.ksi.jinfer.attrstats.objects.AMModel;
+import cz.cuni.mff.ksi.jinfer.attrstats.objects.AttributeMappingId;
 import cz.cuni.mff.ksi.jinfer.attrstats.objects.IdSet;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,12 +29,35 @@ import java.util.List;
  *
  * @author vektor
  */
-public class Identity implements ImprovementHeuristic {
+public class RandomRemove implements ImprovementHeuristic {
+
+  private final double ratio;
+
+  public RandomRemove(final double ratio) {
+    super();
+    if (ratio < 0 || ratio >= 1) {
+      throw new IllegalArgumentException("Invalid ratio: " + ratio);
+    }
+    this.ratio = ratio;
+  }
 
   @Override
   public void start(final AMModel model, final List<IdSet> feasiblePool,
         final HeuristicCallback callback) throws InterruptedException {
-    callback.finished(feasiblePool);
+
+    final List<IdSet> ret = new ArrayList<IdSet>(feasiblePool.size());
+    for (final IdSet idSet : feasiblePool) {
+      final int toRemove = (int)Math.round(idSet.getMappings().size() * ratio);
+
+      final List<AttributeMappingId> mappings = new ArrayList<AttributeMappingId>(idSet.getMappings());
+      for (int i = 0; i < toRemove; i++) {
+        mappings.remove((int)(Math.random() * mappings.size()));
+      }
+
+      ret.add(new IdSet(mappings));
+    }
+
+    callback.finished(ret);
   }
 
 }

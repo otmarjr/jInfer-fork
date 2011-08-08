7e999f75129c80d1dd8053122e6f401cd6a34387
vektor330 <vektor330@gmail.com>
2011-08-08 12:10:44 +0000

    Renaming AttributeStats: OMG

--- jinfer/trunk/jInfer/AutoEditor/src/cz/cuni/mff/ksi/jinfer/autoeditor/automatonvisualizer/layouts/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains layouts for
- * {@link cz.cuni.mff.ksi.jinfer.autoeditor.automatonvisualizer.Visualizer}.
- */
-package cz.cuni.mff.ksi.jinfer.autoeditor.automatonvisualizer.layouts;

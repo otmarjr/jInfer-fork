f2df5cf8438a8b93423eb28312772e0a2528e2be
riacik <riacik@214d1e34-ff19-46a5-8cc8-ebf7b210affd>
2012-02-12 22:26:46 +0000

    Master thesis implementation: Analysis of expression types, part 2.

--- jinfer/branches/thesis-mikula/XQueryAnalyzer/src/cz/cuni/mff/ksi/jinfer/xqueryanalyzer/Type.java
+++ jinfer/branches/thesis-mikula/XQueryAnalyzer/src/cz/cuni/mff/ksi/jinfer/xqueryanalyzer/Type.java
@@ -30,12 +30,33 @@ public class Type {
     PATH;
   }
   
+  public enum XSDAtomicType {
+    FLOAT,
+    DOUBLE,
+    DECIMAL,
+    INTEGER,
+    LONG,
+    INT,
+    SHORT,
+    BYTE,
+    NON_POSITIVE_INTEGER,
+    NEGATIVE_INTEGER,
+    NON_NEGATIVE_INTEGER,
+    POSITIVE_INTEGER,
+    UNSIGNED_LONG,
+    UNSIGNED_BYTE,
+    STRING,
+    BOOLEAN,
+    ANY_TYPE; // TODO rio toto existuje?
+    // TODO rio doplnit
+  }
+  
   private final Category category;
   private final Cardinality cardinality;
   
-  private String buildinTypeName;
+  private XSDAtomicType buildinTypeName;
   
-  public Type(final String buildinTypeName, final Cardinality cardinality) {
+  public Type(final XSDAtomicType buildinTypeName, final Cardinality cardinality) {
     category = Category.BUILT_IN;
     this.buildinTypeName = buildinTypeName;
     this.cardinality = cardinality;
@@ -46,16 +67,16 @@ public class Type {
     cardinality = Cardinality.ONE;
     switch(literalType) {
       case DECIMAL:
-        buildinTypeName = "xs:decimal";
+        buildinTypeName = XSDAtomicType.DECIMAL;
         break;
       case DOUBLE:
-        buildinTypeName = "xs:double";
+        buildinTypeName = XSDAtomicType.DOUBLE;
         break;
       case INTEGER:
-        buildinTypeName = "xs:integer";
+        buildinTypeName = XSDAtomicType.INT;
         break;
       case STRING:
-        buildinTypeName = "xs:string";
+        buildinTypeName = XSDAtomicType.STRING;
     }
   }
   
@@ -66,13 +87,13 @@ public class Type {
     if (itemTypeNode == null) {
       // TODO rio what is this type?
       category = Category.BUILT_IN;
-      buildinTypeName = "xs:anyType";
+      buildinTypeName = XSDAtomicType.ANY_TYPE;
       assert(false);
     } else {
       if (itemTypeNode.getClass().equals(AtomicTypeNode.class)) {
         category = Category.BUILT_IN;
         final AtomicTypeNode itomicTypeNode = (AtomicTypeNode)itemTypeNode;
-        buildinTypeName = itomicTypeNode.getTypeName();
+        buildinTypeName = Type.atomicNameToType(itomicTypeNode.getTypeName());
       } else if (itemTypeNode.getClass().equals(KindTestNode.class)) {
         // TODO rio vymysliet, preskumat
         category = Category.NODE;
@@ -82,7 +103,70 @@ public class Type {
         category = Category.BUILT_IN;
         assert(false);
       }
+    } 
+  }
+  
+  private static XSDAtomicType atomicNameToType(final String atomicTypeName) {
+    if (atomicTypeName.equals("xs:float")) {
+      return XSDAtomicType.FLOAT;
+    } else if (atomicTypeName.equals("xs:double")) {
+      return XSDAtomicType.DOUBLE;
+    } else if (atomicTypeName.equals("xs:decimal")) {
+      return XSDAtomicType.DECIMAL;
+    } else if (atomicTypeName.equals("xs:integer")) {
+      return XSDAtomicType.INTEGER;
+    } else if (atomicTypeName.equals("xs:long")) {
+      return XSDAtomicType.LONG;
+    } else if (atomicTypeName.equals("xs:int")) {
+      return XSDAtomicType.INT;
+    } else if (atomicTypeName.equals("xs:short")) {
+      return XSDAtomicType.SHORT;
+    } else if (atomicTypeName.equals("xs:byte")) {
+      return XSDAtomicType.BYTE;
+    } else if (atomicTypeName.equals("xs:nonPositiveInteger")) {
+      return XSDAtomicType.NON_POSITIVE_INTEGER;
+    } else if (atomicTypeName.equals("xs:negativeInteger")) {
+      return XSDAtomicType.NEGATIVE_INTEGER;
+    } else if (atomicTypeName.equals("xs:nonNegativeInteger")) {
+      return XSDAtomicType.NON_NEGATIVE_INTEGER;
+    } else if (atomicTypeName.equals("xs:positiveInteger")) {
+      return XSDAtomicType.POSITIVE_INTEGER;
+    } else if (atomicTypeName.equals("xs:unsignedLong")) {
+      return XSDAtomicType.UNSIGNED_LONG;
+    } else if (atomicTypeName.equals("xs:unsignedByte")) {
+      return XSDAtomicType.UNSIGNED_BYTE;
+    } else if (atomicTypeName.equals("xs:string")) {
+      return XSDAtomicType.STRING;
+    } else if (atomicTypeName.equals("xs:anyType")) {
+      return XSDAtomicType.ANY_TYPE;
     }
     
+    assert(false);
+    return null;
+  }
+  
+  public boolean isNumeric() {
+    if (category == Category.BUILT_IN) {
+      switch (buildinTypeName) {
+        case FLOAT:
+        case DOUBLE:
+        case DECIMAL:
+        case INTEGER:
+        case LONG:
+        case SHORT:
+        case BYTE:
+        case NON_POSITIVE_INTEGER:
+        case NEGATIVE_INTEGER:
+        case NON_NEGATIVE_INTEGER:
+        case POSITIVE_INTEGER:
+        case UNSIGNED_LONG:
+        case UNSIGNED_BYTE:
+          return true;
+        default:
+          return false;
+      }
+    }
+    
+    return false;
   }
 }

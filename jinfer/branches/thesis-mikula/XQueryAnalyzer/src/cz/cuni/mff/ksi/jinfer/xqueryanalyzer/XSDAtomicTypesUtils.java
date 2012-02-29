d4719c8d02c050fce765d3c29d63b06732f0b5d6
riacik <riacik@214d1e34-ff19-46a5-8cc8-ebf7b210affd>
2012-02-29 23:01:01 +0000

    Master thesis implementation: In progress.

---
+++ jinfer/branches/thesis-mikula/XQueryAnalyzer/src/cz/cuni/mff/ksi/jinfer/xqueryanalyzer/XSDAtomicTypesUtils.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2012 rio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cuni.mff.ksi.jinfer.xqueryanalyzer;
+
+import cz.cuni.mff.ksi.jinfer.base.xqueryanalyzer.types.XSDType;
+
+/**
+ *
+ * @author rio
+ */
+public class XSDAtomicTypesUtils {
+
+  public static XSDType selectMoreSpecific(final XSDType type1, final XSDType type2) {
+    // TODO rio
+    return type1;
+  }
+}

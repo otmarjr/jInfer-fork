b015f3ca5b0291822bcac62d1265caa115e22275
michalklempa <michalklempa@214d1e34-ff19-46a5-8cc8-ebf7b210affd>
2012-03-30 06:04:55 +0000

    Changed default simplifier to greedymdl

--- jinfer/trunk/jInfer/TwoStepSimplifier/src/cz/cuni/mff/ksi/jinfer/twostep/processing/automatonmergingstate/AutomatonMergingStateFactory.java
+++ jinfer/trunk/jInfer/TwoStepSimplifier/src/cz/cuni/mff/ksi/jinfer/twostep/processing/automatonmergingstate/AutomatonMergingStateFactory.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright (C) 2010 anti
- * 
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -40,7 +40,7 @@ import org.openide.util.lookup.ServiceProvider;
 public class AutomatonMergingStateFactory implements ClusterProcessorFactory {
 
   private static final Logger LOG = Logger.getLogger(AutomatonMergingStateFactory.class);
-  /** 
+  /**
    * Canonical name.
    */
   public static final String NAME = "TwoStepClusterProcessorAutomatonMergingState";
@@ -55,7 +55,7 @@ public class AutomatonMergingStateFactory implements ClusterProcessorFactory {
   /**
    * Default {@link AutomatonSimplifier} implementation if none is set.
    */
-  public static final String PROPERTIES_AUTOMATON_SIMPLIFIER_DEFAULT = "TwoStepClusterProcessorAutomatonMergingStateAutomatonSimplifierChained";
+  public static final String PROPERTIES_AUTOMATON_SIMPLIFIER_DEFAULT = "TwoStepClusterProcessorAutomatonMergingStateAutomatonSimplifierGreedyMDL";
   /**
    * Property name (in configuration file) of second submodule - {@link RegexpAutomatonSimplifier}
    */

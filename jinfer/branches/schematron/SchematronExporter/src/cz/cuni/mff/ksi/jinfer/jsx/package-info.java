4a5e84b0c16a0186bec0db4d13200ea7d0d2d771
reseto <reseto@214d1e34-ff19-46a5-8cc8-ebf7b210affd>
2012-07-12 20:05:03 +0000

    Module SchematronExporter for exporting schematron schemas

---
+++ jinfer/branches/schematron/SchematronExporter/src/cz/cuni/mff/ksi/jinfer/jsx/package-info.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2012 reseto
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * Main package of the SchematronExporter module. This module provides an
+ * implementation of the {@link cz.cuni.mff.ksi.jinfer.base.interfaces.inference.SchemaGenerator}
+ * interface. It exports simplified grammar into Schematron schema.
+ */
+package cz.cuni.mff.ksi.jinfer.jsx;
